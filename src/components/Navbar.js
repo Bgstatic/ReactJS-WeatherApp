fe609c80878d472c38da5e37a23eadcdbb523f7c
Bgstatic <bilgehangecici@gmail.com>
2020-10-06 18:54:29 +0300

    Weather details added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,8 +19,45 @@ function Navbar() {
           <hr />
         </div>
       </div>
-      <div className="navbar__details">
-        <div className="navbar__details__cloud"></div>
+      <div className="navbar__details__container">
+        <div className="navbar__details__title">
+          <div className="title">
+            <h3>Weather Details</h3>
+          </div>
+          <div className="arrange"></div>
+        </div>
+        <div className="navbar__details__cloudy">
+          <div className="cloudy">
+            <h3>Cloudy</h3>
+          </div>
+          <div className="cloudy__value">
+            <h3>86%</h3>
+          </div>
+        </div>
+        <div className="navbar__details__Humidity">
+          <div className="humidity">
+            <h3>Humidity</h3>
+          </div>
+          <div className="humidity__value">
+            <h3>62%</h3>
+          </div>
+        </div>
+        <div className="navbar__details__Wind">
+          <div className="wind">
+            <h3>Wind</h3>
+          </div>
+          <div className="wind__value">
+            <h3>8km/h</h3>
+          </div>
+        </div>
+        <div className="navbar__details__Rain">
+          <div className="rain">
+            <h3>Rain</h3>
+          </div>
+          <div className="rain__value">
+            <h3>8mm</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
