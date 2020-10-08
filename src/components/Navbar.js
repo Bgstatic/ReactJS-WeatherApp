3a785d6adf446fda29c6b405f151df84e93aca09
Bgstatic <bilgehangecici@gmail.com>
2020-10-08 16:00:41 +0300

    WeatherDetails.js integration.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import WeatherDetails from "./WeatherDetails";
 import Input from "@material-ui/core/Input";
 import SearchIcon from "@material-ui/icons/Search";
 import "../styles/Navbar.css";
@@ -16,49 +17,10 @@ function Navbar() {
           <h3>Manchester</h3>
           <h3>New York</h3>
           <h3>California</h3>
-          <hr />
         </div>
+        <div className="arrange__cities"></div>
       </div>
-      <div className="navbar__details__container">
-        <div className="navbar__details__title">
-          <div className="title">
-            <h3>Weather Details</h3>
-          </div>
-          <div className="arrange"></div>
-        </div>
-        <div className="navbar__details__cloudy">
-          <div className="cloudy">
-            <h3>Cloudy</h3>
-          </div>
-          <div className="cloudy__value">
-            <h3>86%</h3>
-          </div>
-        </div>
-        <div className="navbar__details__Humidity">
-          <div className="humidity">
-            <h3>Humidity</h3>
-          </div>
-          <div className="humidity__value">
-            <h3>62%</h3>
-          </div>
-        </div>
-        <div className="navbar__details__Wind">
-          <div className="wind">
-            <h3>Wind</h3>
-          </div>
-          <div className="wind__value">
-            <h3>8km/h</h3>
-          </div>
-        </div>
-        <div className="navbar__details__Rain">
-          <div className="rain">
-            <h3>Rain</h3>
-          </div>
-          <div className="rain__value">
-            <h3>8mm</h3>
-          </div>
-        </div>
-      </div>
+      <WeatherDetails />
     </div>
   );
 }
