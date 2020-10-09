8c131e7d13659db7e16999d8f98ea04212d1d96c
Bgstatic <bilgehangecici@gmail.com>
2020-10-09 18:11:34 +0300

    Passing data from child to parent.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Weather from "./components/Weather";
 import "./styles/App.css";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const getData = (data) => {
+    setData(data);
+  };
   return (
     <div className="App">
-      <Weather />
-      <Navbar />
+      <Weather data={data} />
+      <Navbar getData={getData} />
     </div>
   );
 }
