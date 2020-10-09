3302d7fd05d9f8fe833c25c3e1fe5735ad9b5397
Bgstatic <bilgehangecici@gmail.com>
2020-10-09 18:12:23 +0300

    fetchWeather.js added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherDetails from "./WeatherDetails";
+import { fetchWeather } from "../services/fetchWeather";
 import Input from "@material-ui/core/Input";
 import SearchIcon from "@material-ui/icons/Search";
 import "../styles/Navbar.css";
 
-function Navbar() {
+function Navbar({ getData }) {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const WeatherData = await fetchWeather(query);
+      setWeather(WeatherData);
+      setQuery("");
+    }
+  };
+  getData(weather);
+  console.log(weather.weather?.[0].icon);
   return (
     <div className="weather__navbar">
       <div className="weather__input__container">
-        <Input className="navbar__input" placeholder="Another Location" />
+        <Input
+          className="navbar__input"
+          placeholder="Another Location"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
         <SearchIcon className="navbar__searchIcon" />
       </div>
       <div className="navbar__cities__container">
         <div className="cities">
-          <h3>Birmingham</h3>
+          <h3 onClick={(e) => setQuery(e.target.value)}>Birmingham</h3>
           <h3>Manchester</h3>
           <h3>New York</h3>
           <h3>California</h3>
         </div>
         <div className="arrange__cities"></div>
       </div>
-      <WeatherDetails />
+      <WeatherDetails weather={weather} />
     </div>
   );
 }
