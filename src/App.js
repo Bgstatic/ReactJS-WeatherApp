f93565f90af6115a33ded7bdf75a25e30da5beb0
Bgstatic <bilgehangecici@gmail.com>
2020-10-11 15:01:07 +0300

    Set default weather.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Weather from "./components/Weather";
+import { fetchWeather } from "./services/fetchWeather";
 import "./styles/App.css";
 
 function App() {
   const [data, setData] = useState({});
+  const [defaultData, setDefaultData] = useState({});
+
+  const getDefault = async () => {
+    const WeatherData = await fetchWeather("London");
+    setDefaultData(WeatherData);
+  };
+
+  useEffect(() => {
+    getDefault();
+  }, []);
 
   const getData = (data) => {
     setData(data);
   };
   return (
     <div className="App">
-      <Weather data={data} />
-      <Navbar getData={getData} />
+      <Weather data={data} defaultData={defaultData} />
+      <Navbar getData={getData} defaultData={defaultData} />
     </div>
   );
 }
