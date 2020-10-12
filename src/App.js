399a188d0437d1885bc41f83ee422a7a8e06cbe5
Bgstatic <bilgehangecici@gmail.com>
2020-10-12 14:16:52 +0300

    fetchPhotos integration.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Weather from "./components/Weather";
+import { fetchPhotos } from "./services/fetchPhotos";
 import { fetchWeather } from "./services/fetchWeather";
 import "./styles/App.css";
 
 function App() {
   const [data, setData] = useState({});
+  const [image, setImage] = useState({});
+  const [defaultImage, setDefaultImage] = useState({});
   const [defaultData, setDefaultData] = useState({});
 
   const getDefault = async () => {
@@ -13,17 +16,35 @@ function App() {
     setDefaultData(WeatherData);
   };
 
+  const getDefaultImage = async () => {
+    const preImage = await fetchPhotos("London");
+    setDefaultImage(preImage.results);
+  };
+
   useEffect(() => {
     getDefault();
+    getDefaultImage();
   }, []);
-
   const getData = (data) => {
     setData(data);
   };
+
+  const getImage = (data) => {
+    setImage(data);
+  };
+  const backgroundImageLink = image[0]
+    ? image[Math.floor(Math.random() * 10)]?.urls?.full
+    : defaultImage[Math.floor(Math.random() * 10)]?.urls?.full;
+  console.log(backgroundImageLink);
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${backgroundImageLink})`,
+      }}
+    >
       <Weather data={data} defaultData={defaultData} />
-      <Navbar getData={getData} defaultData={defaultData} />
+      <Navbar getData={getData} getImage={getImage} defaultData={defaultData} />
     </div>
   );
 }
